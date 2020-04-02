00c25b03be045ad8dbd8c38f51755679a111291e
Aleksej <avoroncovs@gmail.com>
2020-04-02 22:50:33 +0300

    acceptance test

---
+++ src/test/acceptance/java/lv/phonenumbervalidator/PhoneNumberValidatorTest.java
@@ -0,0 +1,67 @@
+package lv.phonenumbervalidator;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lv.phonenumbervalidator.dto.PhoneNumberDTO;
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.io.File;
+import java.io.IOException;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ContextConfiguration(classes = PhoneNumberValidatorTest.TestConfig.class)
+@AutoConfigureMockMvc
+public class PhoneNumberValidatorTest {
+
+    @TestConfiguration
+    public static class TestConfig {
+        @Bean
+        @Primary
+        public Connection countryCodeUrlConnection() throws IOException {
+            Connection countryCodeUrlConnection = Mockito.mock(Connection.class);
+            when(countryCodeUrlConnection.get()).thenReturn(Jsoup.parse(new File("src/test/resources/html/List_of_country_calling_codes.html"), "UTF-8"));
+            return countryCodeUrlConnection;
+        }
+    }
+
+    @Autowired
+    MockMvc mvc;
+
+    @Test
+    public void shouldIdentifyLatvianNumber() throws Exception {
+        PhoneNumberDTO phoneNumberDTO = preparePhoneNumberDTO("+37122222222");
+
+        mvc.perform(MockMvcRequestBuilders
+                .post("/api/v1/validatePhoneNumber")
+                .content(new ObjectMapper().writeValueAsString(phoneNumberDTO))
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.country").value("Latvia"));
+    }
+
+    private PhoneNumberDTO preparePhoneNumberDTO(String phoneNumber) {
+        PhoneNumberDTO phoneNumberDTO = new PhoneNumberDTO();
+        phoneNumberDTO.setPhoneNumber(phoneNumber);
+        return phoneNumberDTO;
+    }
+
+}
--- src/test/integration/java/lv/phonenumbervalidator/repository/CountryCodeRepositoryTest.java
+++ src/test/integration/java/lv/phonenumbervalidator/repository/CountryCodeRepositoryTest.java
@@ -3,14 +3,17 @@ package lv.phonenumbervalidator.repository;
 import lv.phonenumbervalidator.entity.CountryCode;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 
+@RunWith(SpringRunner.class)
 @DataJpaTest
 public class CountryCodeRepositoryTest {
 
