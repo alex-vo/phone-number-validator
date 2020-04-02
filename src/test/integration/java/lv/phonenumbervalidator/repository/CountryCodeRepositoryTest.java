81dea5041a68aa3d4bf6383b57159babdbbf9d1f
Aleksej <avoroncovs@gmail.com>
2020-04-02 16:21:38 +0300

    integration test for CountryCodeRepository

---
+++ src/test/integration/java/lv/phonenumbervalidator/repository/CountryCodeRepositoryTest.java
@@ -0,0 +1,57 @@
+package lv.phonenumbervalidator.repository;
+
+import lv.phonenumbervalidator.entity.CountryCode;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+@DataJpaTest
+public class CountryCodeRepositoryTest {
+
+    @Autowired
+    CountryCodeRepository countryCodeRepository;
+
+    @BeforeEach
+    public void setup() {
+        countryCodeRepository.saveAll(List.of(
+                prepareCountryCode("Usa", "+1"),
+                prepareCountryCode("Canada", "+1"),
+                prepareCountryCode("Bahamas", "+1242")
+        ));
+    }
+
+    @Test
+    public void shouldFindCountryWithLongestMatchingCountryCode() {
+        List<String> result = countryCodeRepository.findCountryCodeForPhoneNumber("+12420");
+
+        assertThat(result, contains("Bahamas"));
+    }
+
+    @Test
+    public void shouldFindCountryWithLongestMatchingCountryCodeButNotEntirelyMatching() {
+        List<String> result = countryCodeRepository.findCountryCodeForPhoneNumber("+1242");
+
+        assertThat(result, contains("Usa", "Canada"));
+    }
+
+    @Test
+    public void shouldFailToFindCountryByNonExistingCode() {
+        List<String> result = countryCodeRepository.findCountryCodeForPhoneNumber("+82828");
+
+        assertThat(result, is(empty()));
+    }
+
+    private CountryCode prepareCountryCode(String country, String code) {
+        CountryCode countryCode = new CountryCode();
+        countryCode.setCountry(country);
+        countryCode.setCode(code);
+        return countryCode;
+    }
+
+}
